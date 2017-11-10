5e52c19c9db96ff09d1464851f017f3e737a0429
ehoeg <hoeg.erik@gmail.com>
2017-11-10 17:15:28 -0600

    Initial Commit

---
+++ app/src/main/java/com/example/hoege1/movietime/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.hoege1.movietime;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
