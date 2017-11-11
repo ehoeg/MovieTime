bd4da43c19dd7e3b2202ebbf4ae4bd0c68d91b8a
ehoeg <hoeg.erik@gmail.com>
2017-11-10 18:41:31 -0600

    Added FetchMovieData class

--- app/src/main/java/com/example/hoege1/movietime/MainActivity.java
+++ app/src/main/java/com/example/hoege1/movietime/MainActivity.java
@@ -5,11 +5,14 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity
 {
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+
+        FetchMovieData movieTask = new FetchMovieData();
+        movieTask.execute();
+
         setContentView(R.layout.activity_main);
     }
 }
